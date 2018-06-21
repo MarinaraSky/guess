9786906774422ced333480296870f831a74e02a0
Jack Spence <jack.c.spence@gmail.com>
2018-06-21 18:25:40 -0400

    Restructured functions to call checkInput from inputVal to clean up main.

--- guess.py
+++ guess.py
@@ -2,49 +2,55 @@
 import random
 
 
-def inputVal(check):
+def inputVal(check, rand, guessCount):
     '''Function used to return either the user selected integer within valid
     range or print out an error'''
     invalidOutput = "{} is invalid input"
     if check.isdigit():     # Checks if theres non numbers in string
         check = int(check)  # Converts to int object
         if (1 <= check <= 100):     # Verifies its within valid range
-            return check
+            return checkInput(check, rand, guessCount)
         else:
             print(invalidOutput.format(check))
-            return False
+            return True
     else:
         print(invalidOutput.format(check))
-        return False
+        return True
 
 
-def checkInput(selection, rand):
+def checkInput(selection, rand, guessCount):
     '''Used to check if they guess the number right and return
     high or low statement'''
+    guessCount += 1     # Only increments with valid input
     if selection == rand:
-        return True
+        return False, guessCount
     else:
         if selection < rand:
-            return print(selection, "is to low. ")
+            print(selection, "is to low. ")
+            return True, guessCount
         else:
-            return print(selection, "is to high. ")
+            print(selection, "is to high. ")
+            return True, guessCount
 
 
 def main():
     rand = random.randint(1, 100)
     guessCount = 0
-    while True:     # Loop until they win
+    playing = True
+    winning = "YOU WON in {} guesses."
+    while playing:     # Loop until they win
         try:
             selection = input("Please guess a number between 1 and 100: ")
         except (KeyboardInterrupt, EOFError):
             print()
             break
-        selection = inputVal(selection)
-        if selection:   # Wont be satisfied if check was false
-            guessCount += 1     # Only increments if valid guess
-            if checkInput(selection, rand):     # When right number is guessed
-                print("YOU WON in ", guessCount, "guesses")
-                break
+        # Playing will be False unless they win, guessCount will be returned
+        playing, guessCount = inputVal(selection, rand, guessCount)
+        if playing is False:    # Win Condition
+            if guessCount == 1:
+                print(winning.format(guessCount).replace("guesses", "guess"))
+            else:
+                print(winning.format(guessCount))
 
 if __name__ == "__main__":
     main()
