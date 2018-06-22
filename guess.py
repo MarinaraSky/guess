63153e164e19c0e8134d50f957cceb3389d63490
Jack Spence <jack.c.spence@gmail.com>
2018-06-21 23:06:12 -0400

    Added returning of number that was lied on. Changed returnList to initilized earlier than before.

--- guess.py
+++ guess.py
@@ -3,15 +3,14 @@ import random
 import sys
 
 
-def valInput(playerGuess, rand, guessCount, ulam):
+def valInput(playerGuess, returnList, rand):
     '''Function used to return either the user selected integer within valid
     range or print out an error'''
     invalidOutput = "{} is invalid input"
-    returnList = [True, guessCount, ulam]
     if playerGuess.isdigit():     # Checks if theres non numbers in string
         playerGuess = int(playerGuess)  # Converts to int object
         if (1 <= playerGuess <= 100):     # Verifies its within valid range
-            return playGame(playerGuess, rand, returnList, ulam)
+            return playGame(playerGuess, rand, returnList)
         else:
             invalidInput(playerGuess)
             return returnList
@@ -26,7 +25,7 @@ def invalidInput(playerGuess):
     print(invalidOut.format(playerGuess))
 
 
-def playGame(playerGuess, rand, returnList, ulam):
+def playGame(playerGuess, rand, returnList):
     '''Used to check if they guess the number right and return
     high or low statement'''
     returnList[1] += 1     # Only increments with valid input
@@ -35,17 +34,18 @@ def playGame(playerGuess, rand, returnList, ulam):
     if playerGuess == rand:
         returnList[0] = False
     else:
-        if ulam:
-            lying = amILying(returnList[1])
+        if returnList[2]:   # This is checking if Ulam's game is enabled
+            lying = amILying(returnList[1])  # Using the attempt number to lie
         else:
-            lying = ulam
+            lying = returnList[2]   # Ulam's not enabled
         if lying:
             if playerGuess < rand:
                 print(high.format(playerGuess))
-                returnList[2] = False
+                returnList[3] = high.format(playerGuess).replace("is", "was")
             elif playerGuess > rand:
                 print(low.format(playerGuess))
-                returnList[2] = False
+                returnList[3] = low.format(playerGuess).replace("is", "was")
+            returnList[2] = False   # Turning off Ulam's mode
         elif playerGuess < rand:
             print(low.format(playerGuess))
         elif playerGuess > rand:
@@ -54,22 +54,30 @@ def playGame(playerGuess, rand, returnList, ulam):
 
 
 def amILying(currentGuess):
-    ulamRand = random.randint(1, currentGuess)
+    '''Used to check if I'm going to lie on this check, used 3
+    arbitrarily to lessen the chance of lying on the first couple
+    of guesses'''
+    ulamRand = random.randint(currentGuess, currentGuess + 3)
     if ulamRand == currentGuess:
-        print("I'm Lying")
         return True
 
 
 def main():
     # Checks if -u was passed to enable lying
     if len(sys.argv) == 2 and sys.argv[1] == "-u":
+        print("Ulam's game Enabled")
         ulam = True
     else:
+        print("Ulam's game Not Enabled")
         ulam = False
     rand = random.randint(1, 100)
     count = 0
     playing = True
     winning = "YOU WON in {} guesses."
+    lied = ""
+    # returnList is used to track if you're still playing, the number
+    # of valid attemps, if ulam is in effect, and the lied message
+    returnList = [True, count, ulam, lied]
     while playing:     # Loop until they win
         try:
             playerGuess = input("Please guess a number between 1 and 100: ")
@@ -81,12 +89,16 @@ def main():
             else:
                 continue
         # Playing will be False unless they win, guessCount will be returned
-        playing, count, ulam = valInput(playerGuess, rand, count, ulam)
+        playing, count, ulam, lied = valInput(playerGuess, returnList, rand)
         if playing is False:    # Win Condition
             if count == 1:
                 print(winning.format(count).replace("guesses", "guess"))
             else:
                 print(winning.format(count))
+            if lied:
+                print("When I said", lied, "that was a lie")
+            elif len(sys.argv) == 2:
+                print("I did not lie")
 
 if __name__ == "__main__":
     main()
